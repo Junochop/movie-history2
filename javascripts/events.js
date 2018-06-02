d9f3306293c77bc7c565ac86fe8354dcf7486cd3
Juno Lee <lejuno80@gmail.com>
2018-06-02 11:55:09 -0500

    added delete

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,38 +1,41 @@
-const tmbd = require('./tmdb');
+/* eslint camelcase: 0 */
+
+const tmdb = require('./tmdb');
 const firebaseApi = require('./firebaseApi');
 const dom = require('./dom');
 
-const mylinks = () => {
+const myLinks = () => {
   $(document).click((e) => {
-    if (e.target.id === 'authlink') {
+    if (e.target.id === 'authenticate') {
       $('#myMovies').addClass('hide');
       $('#search').addClass('hide');
       $('#authScreen').removeClass('hide');
-    } else if (e.target.id === 'movielink') {
-      $('#authScreen').addClass('hide');
-      $('#search').addClass('hide');
+    } else if (e.target.id === 'mine') {
       $('#myMovies').removeClass('hide');
-      // call get movies event
-      getAllMoviesEvent();
-    } else if (e.target.id === 'searchlink') {
+      $('#search').addClass('hide');
       $('#authScreen').addClass('hide');
+      // call the getMoviesEvent
+      getAllMoviesEvent();
+    } else if (e.target.id === 'navSearch') {
       $('#myMovies').addClass('hide');
       $('#search').removeClass('hide');
+      $('#authScreen').addClass('hide');
     }
   });
 };
 
 const pressEnter = () => {
-  // keyPress Event
+  // big old keypress event
   $(document).keypress((e) => {
     if (e.key === 'Enter') {
       const searchWords = $('#searchBar').val().replace(' ', '%20');
-      tmbd.showResults(searchWords);
-    };
+      tmdb.showResults(searchWords);
+    }
   });
 };
+
 const saveMovieToWishlistEvent = () => {
-  $(document).on('click', '.addMovietoWishlist', (e) => {
+  $(document).on('click', '.addMovieToWishlist', (e) => {
     const movieToAddCard = $(e.target).closest('.movie');
     const movieToAdd = {
       title: movieToAddCard.find('.movie-title').text(),
@@ -41,7 +44,7 @@ const saveMovieToWishlistEvent = () => {
       rating: 0,
       isWatched: false,
     };
-    firebaseApi.saveMovieToWishList(movieToAdd)
+    firebaseApi.saveMovieToWishlist(movieToAdd)
       .then(() => {
         movieToAddCard.remove();
       })
@@ -54,17 +57,68 @@ const saveMovieToWishlistEvent = () => {
 const getAllMoviesEvent = () => {
   firebaseApi.getAllMovies()
     .then((moviesArray) => {
-      dom.domString(moviesArray, tmbd.getImageConfig(), 'savedMovies');
+      dom.domString(moviesArray, tmdb.getImageConfig(), 'savedMovies', true);
     })
     .catch((error) => {
-      console.error('error in get all movie', error);
+      console.error('error in get all movies', error);
     });
 };
 
+const deleteMovieFromFirebase = () => {
+  $(document).on('click', '.deleteMovieFromCollectionEvent', (e) => {
+    const movieToDeleteId = $(e.target).closest('.movie').data('firebaseId');
+    firebaseApi.deleteMovieFromDb(movieToDeleteId)
+      .then(() => {
+        getAllMoviesEvent();
+      })
+      .catch((error) => {
+        console.error('error from delete movie', error);
+      });
+  });
+};
+
+const updateMovieEent = () => {
+  $(document).on('click', '.updateMovieToWatched', (e) => {
+    const movieToUpdateId = $(e.target).closest('.movie').data('firebaseId');
+    const movieToUpdateCard = $(e.target).closest('.movie');
+    const updatedMovie = {
+      title: movieToUpdateCard.find('.movie-title').text(),
+      overview: movieToUpdateCard.find('.movie-overview').text(),
+      'poster_path': movieToUpdateCard.find('img').data('poster'),
+      rating: 0,
+      isWatched: true,
+    };
+    firebaseApi.updateMovieToWatchedInDb(updatedMovie, movieToUpdateId)
+      .then(() => {
+        getAllMoviesEvent();
+      })
+      .catch((error) => {
+        console.error('error in update movie', error);
+      });
+  });
+};
+
 const initializer = () => {
-  mylinks();
+  myLinks();
   pressEnter();
   saveMovieToWishlistEvent();
+  deleteMovieFromFirebase();
+  updateMovieEent();
+  filterEvents();
+};
+
+const filterEvents = () => {
+  $('#filterButtons').on('click', (e) => {
+    const classList = e.target.classList;
+    if (classList.contains('wishlist')) {
+      //  show only isWatched: false;
+    } else if (classList.contains('watched')) {
+
+    } else {
+      //  give me everrthing
+      getAllMoviesEvent();
+    }
+  });
 };
 
 module.exports = {
---
+++ javascripts/firebaseApi.js
@@ -0,0 +1,128 @@
+let firebaseConfig = {};
+
+const setConfig = (fbConfig) => {
+  firebaseConfig = fbConfig;
+};
+
+const saveMovieToWishlist = (newMovie) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: 'POST',
+      url: `${firebaseConfig.databaseURL}/movies.json`,
+      data: JSON.stringify(newMovie),
+    })
+      .done((uniqueKey) => {
+        resolve(uniqueKey);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getAllMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getWatchedMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json?orderBy="isWatched"&equalTo=true`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getWishlistMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json?orderBy="isWatched"&equalTo=false`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const deleteMovieFromDb = (movieId) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: 'DELETE',
+      url: `${firebaseConfig.databaseURL}/movies/${movieId}.json`,
+    })
+      .done(() => {
+        resolve();
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const updateMovieToWatchedInDb = (updatedMovie, movieId) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: 'PUT',
+      url: `${firebaseConfig.databaseURL}/movies/${movieId}.json`,
+      data: JSON.stringify(updatedMovie),
+    })
+      .done((modifiedMovie) => {
+        resolve(modifiedMovie);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+module.exports = {
+  setConfig,
+  saveMovieToWishlist,
+  getAllMovies,
+  getWatchedMovies,
+  getWishlistMovies,
+  deleteMovieFromDb,
+  updateMovieToWatchedInDb,
+};
