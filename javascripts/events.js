95d8e7af862909b372a176674943b6f45dd4fb4b
Juno Lee <lejuno80@gmail.com>
2018-06-04 21:04:41 -0500

    auth2 added

--- javascripts/events.js
+++ javascripts/events.js
@@ -27,7 +27,7 @@ const myLinks = () => {
 const pressEnter = () => {
   // big old keypress event
   $(document).keypress((e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && !$('#search').hasClass('hide')) {
       const searchWords = $('#searchBar').val().replace(' ', '%20');
       tmdb.showResults(searchWords);
     }
@@ -64,6 +64,26 @@ const getAllMoviesEvent = () => {
     });
 };
 
+const getWishlistMoviesEvent = () => {
+  firebaseApi.getWishlistMovies()
+    .then((moviesArray) => {
+      dom.domString(moviesArray, tmdb.getImageConfig(), 'savedMovies', true);
+    })
+    .catch((error) => {
+      console.error('error in get all movies', error);
+    });
+};
+
+const getWatchedMovies = () => {
+  firebaseApi.getWatchedMovies()
+    .then((moviesArray) => {
+      dom.domString(moviesArray, tmdb.getImageConfig(), 'savedMovies', true);
+    })
+    .catch((error) => {
+      console.error('error in get watched movies', error);
+    });
+};
+
 const deleteMovieFromFirebase = () => {
   $(document).on('click', '.deleteMovieFromCollectionEvent', (e) => {
     const movieToDeleteId = $(e.target).closest('.movie').data('firebaseId');
@@ -77,7 +97,7 @@ const deleteMovieFromFirebase = () => {
   });
 };
 
-const updateMovieEent = () => {
+const updateMovieEvent = () => {
   $(document).on('click', '.updateMovieToWatched', (e) => {
     const movieToUpdateId = $(e.target).closest('.movie').data('firebaseId');
     const movieToUpdateCard = $(e.target).closest('.movie');
@@ -98,29 +118,63 @@ const updateMovieEent = () => {
   });
 };
 
-const initializer = () => {
-  myLinks();
-  pressEnter();
-  saveMovieToWishlistEvent();
-  deleteMovieFromFirebase();
-  updateMovieEent();
-  filterEvents();
-};
-
 const filterEvents = () => {
   $('#filterButtons').on('click', (e) => {
     const classList = e.target.classList;
     if (classList.contains('wishlist')) {
-      //  show only isWatched: false;
+      // show only isWatched: false
+      getWishlistMoviesEvent();
     } else if (classList.contains('watched')) {
-
+      // show only if isWatched: true
+      getWatchedMovies();
     } else {
-      //  give me everrthing
       getAllMoviesEvent();
     }
   });
 };
 
+const authEvents = () => {
+  $('#signin-btn').click((e) => {
+    e.preventDefault();
+    const email = $('#inputEmail').val();
+    const pass = $('#inputPassword').val();
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  });
+
+  $('#register-link').click(() => {
+    $('#login-form').addClass('hide');
+    $('#registration-form').removeClass('hide');
+  });
+
+  $('#signin-link').click(() => {
+    $('#login-form').removeClass('hide');
+    $('#registration-form').addClass('hide');
+  });
+
+  $('#logout').click(() => {
+    firebase.auth().signOut().then(() => {
+    }).catch((error) => {
+      // An error happened.
+      console.error(error);
+    });
+  });
+};
+
+const initializer = () => {
+  myLinks();
+  pressEnter();
+  saveMovieToWishlistEvent();
+  deleteMovieFromFirebase();
+  updateMovieEvent();
+  filterEvents();
+  authEvents();
+};
+
 module.exports = {
   initializer,
+  getAllMoviesEvent,
 };
